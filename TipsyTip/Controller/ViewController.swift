2af959c4f6ee7f226c9b3b08dddb097122626c8d
Gerson <gerson.lcc@hotmail.com>
2017-09-27 17:39:07 +0100

    IBAction for textfield and slider

--- TipsyTip/Controller/ViewController.swift
+++ TipsyTip/Controller/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var tipPercentageSlider: UISlider!
@@ -17,13 +17,19 @@ class ViewController: UIViewController {
     @IBOutlet weak var totalLbl: UILabel!
     @IBOutlet weak var tipLbl: UILabel!
     
+    var tip = TipModel(billAmount: 0.0, tipPercent: 0.0)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
 
+    @IBAction func billAmountChanged(_ sender: UITextField) {
+        print("My value changed")
+    }
     
+    @IBAction func tipPercentageChanged(_ sender: UISlider) {
+        print(tipPercentageSlider.value)
+    }
     
-
 }
 
