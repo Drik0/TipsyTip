b0debd781ae70e85f823708c3d758ca87cb1efe7
Gerson <gerson.lcc@hotmail.com>
2017-09-27 16:26:54 +0100

    Model created

---
+++ TipsyTip/Model/TipModel.swift
@@ -0,0 +1,13 @@
+//
+//  TipModel.swift
+//  TipsyTip
+//
+//  Created by Gerson Costa on 27/09/17.
+//  Copyright © 2017 Gerson Costa. All rights reserved.
+//
+
+import Foundation
+
+class TipModel {
+    
+}
