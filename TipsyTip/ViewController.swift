2b8e925a51f31dfac40a84e81f384722a64babf2
Gerson <gerson.lcc@hotmail.com>
2017-09-27 14:46:29 +0100

    Initial Commit

---
+++ TipsyTip/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TipsyTip
+//
+//  Created by Gerson Costa on 27/09/17.
+//  Copyright © 2017 Gerson Costa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
